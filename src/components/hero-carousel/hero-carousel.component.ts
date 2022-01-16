f96d6a67458b90b0fe50d098342157cdb348328b
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2022-01-16 17:30:08 +0000

    feat: add other hero imgs, but hidden

--- src/components/hero-carousel/hero-carousel.component.ts
+++ src/components/hero-carousel/hero-carousel.component.ts
@@ -1,7 +1,7 @@
 import './hero-carousel.style.css';
 import PizzaPrep from '../../assets/pizza-prep.jpg';
-// import PizzaReady from '../../assets/pizza-ready.jpg';
-// import PizzaServe from '../../assets/pizza-serve.jpg';
+import PizzaReady from '../../assets/pizza-ready.jpg';
+import PizzaServe from '../../assets/pizza-serve.jpg';
 
 export default function HeroCarousel(): Node {
   'use strict';
@@ -9,16 +9,27 @@ export default function HeroCarousel(): Node {
   // TODO: Add other hero images in timed sliding carousel
   // TODO: Add overlay to carousel for short description text
 
-  // const HERO_IMAGES: any[] = [PizzaPrep, PizzaReady, PizzaServe];
+  const HERO_IMAGES: any[] = [PizzaPrep, PizzaReady, PizzaServe];
 
   const heroCarouselElem: HTMLDivElement = document.createElement('div');
   heroCarouselElem.classList.add('hero-carousel');
 
-  const heroImgElem = new Image();
-  heroImgElem.src = PizzaPrep;
-  heroImgElem.classList.add('hero');
+  function createHeroImgElem(source: any, idx: number): HTMLDivElement {
+    const heroImgContainer: HTMLDivElement = document.createElement('div');
+    const heroImgElem = new Image();
+    heroImgElem.src = source;
+    heroImgElem.classList.add('hero');
 
-  heroCarouselElem.append(heroImgElem);
+    if (idx === 0) heroImgElem.classList.add('active');
+
+    heroImgContainer.append(heroImgElem);
+
+    return heroImgContainer;
+  }
+
+  const heroImgElems: HTMLDivElement[] = HERO_IMAGES.map(createHeroImgElem);
+
+  heroCarouselElem.append(...heroImgElems);
 
   return heroCarouselElem;
 }
