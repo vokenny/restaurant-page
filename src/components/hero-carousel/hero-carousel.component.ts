94f903128a45609b10f38b80a6d295d309f13cf1
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2022-01-24 17:12:40 +0000

    refactor(hero): move actions out to hero controller

--- src/components/hero-carousel/hero-carousel.component.ts
+++ src/components/hero-carousel/hero-carousel.component.ts
@@ -1,13 +1,12 @@
+import HeroCarouselController from './hero-carousel.controller';
 import './hero-carousel.style.css';
-import PizzaPrep from '../../assets/pizza-prep.jpg';
-import PizzaReady from '../../assets/pizza-ready.jpg';
-import PizzaServe from '../../assets/pizza-serve.jpg';
+import { HERO_IMAGES } from './hero-images';
 
 export default function HeroCarousel(): Node {
   // TODO: Timed sliding carousel for hero images
   // TODO: Add overlay to carousel for short description text
 
-  const HERO_IMAGES: any[] = [PizzaPrep, PizzaReady, PizzaServe];
+  const heroCtrl = HeroCarouselController();
 
   const heroCarouselElem: HTMLDivElement = document.createElement('div');
   heroCarouselElem.classList.add('hero-carousel');
@@ -20,31 +19,9 @@ export default function HeroCarousel(): Node {
 
     if (idx === 0) heroImgElem.classList.add('active');
 
-    heroImgElem.addEventListener('click', (evt) => showNextImage(evt));
+    heroImgElem.addEventListener('click', (evt) => heroCtrl.showNextImage(evt));
 
     return heroImgElem;
-  }
-
-  function calcNextId(currentId: string): string {
-    const currId: number = parseInt(currentId);
-
-    return currId === HERO_IMAGES.length - 1 ? '0' : `${currId + 1}`;
-  }
-
-  // TODO: Move this to a controller
-  function showNextImage(evt: MouseEvent): void {
-    const currImg: HTMLElement = evt.target as HTMLElement;
-
-    if (!currImg.dataset.id)
-      throw new Error('data-id attr not found on clicked element');
-
-    const nextId: string = calcNextId(currImg.dataset.id);
-    const nextImg: HTMLElement = document.querySelector(
-      `.hero[data-id="${nextId}"]`
-    ) as HTMLElement;
-
-    currImg.classList.remove('active');
-    nextImg.classList.add('active');
   }
 
   const heroImgElems: HTMLDivElement[] = HERO_IMAGES.map(createHeroImgElem);
